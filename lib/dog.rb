d161c662d69326c60433d6b4405e6863de653267
Nunny Reyes <nunnyreyes@gmail.com>
2020-08-03 18:23:22 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog 
+  def bark
+  puts "Woof!"
+end
+
+def sit 
+  puts "The Dog is sitting"
+end 
+end
+
+# def bark
+#   puts "Woofs"
+# end
+
+# def sit 
+#   puts "The Dog is sitting"
+# end 
+
+fido = Dog.new
+fido.bark
+fido.sit
